90e7f811ad041c6a06a9b4bd20d40625f1cf174c
Elias Kuiter <info@elias-kuiter.de>
2013-12-19 21:40:50 +0100

    Rails-Projekt erstellt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tranzfiction::Application.config.secret_key_base = '952a97e821d9a3a05107e24287631811d9cd3a927d793384b65dfc98f1df79ed6cba59bf67cfa209f550a386cb2799c815c6dec0b86efe2cadcf2fe86fc2757e'
