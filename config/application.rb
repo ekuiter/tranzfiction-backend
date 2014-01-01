08567fc43ddceee409f3e12e325ba921bfeafb01
Elias Kuiter <info@elias-kuiter.de>
2014-01-01 15:35:40 +0100

    Models - Unter-Unterverzeichnisse

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Tranzfiction
     config.i18n.default_locale = :de
     
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}', '{**}')]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ module Actions
       create_city:     { path: "city/create", to: "city#create", desc: "Erstellt eine neue Stadt. Der Name wird als ?city[name]=<name> angehängt." },
       city:            { path: "city/:id", to: "city#show", desc: "Zeigt die Stadt mit der ID <city_id> an." },
       buildings:       { path: "city/:city_id/building", to: "building#index", desc: "Zeigt alle Gebäude der Stadt <city_id> an." },
-      create_building: { path: "city/:city_id/building/create", to: "building#create", desc: "Erstellt ein neues Gebäude in der Stadt <id> mit dem Gebäudetyp ?building[type]=<type>. *" },
+      create_building: { path: "city/:city_id/building/create", to: "building#create", desc: "Erstellt ein neues Gebäude in der Stadt <id> mit dem Gebäudetyp ?building[type]=<type>." },
       building:        { path: "city/:city_id/building/:building_id", to: "building#show", desc: "Zeigt das Gebäude <building_id> in der Stadt <city_id> an." },
     }
   end
